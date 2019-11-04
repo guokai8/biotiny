9ea104a26d4e1737ab0e0530dba934ad1e0894bf
bioguo <guokai8@gmail.com>
2019-11-04 08:57:03 -0600

    Update cor_test.r

--- R/cor_test.r
+++ R/cor_test.r
@@ -1,1 +1,7 @@
-cor_test<-function(x,use.colnames=T){
+cor_test<-function(x, use.colnames = T){
+  if(use.colnames == TRUE){
+    comb <- expand.grid(colnames(x),colnames(x))
+    }
+  res <- pmap(comb, ~cor.test(x[[.x]],x[[.y]]))%>%map_df(tidy)%>%cbind(comb,.)
+  return(res)
+  }
