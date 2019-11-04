b37ae8301749db0170e678aaf08bd7115f23010b
bioguo <guokai8@gmail.com>
2019-11-04 09:01:58 -0600

    Update cor_test.r

--- R/cor_test.r
+++ R/cor_test.r
@@ -3,9 +3,14 @@
 ##' @importFrom purrr map_df
 ##' @importFrom purrr pmap
 ##' @importFrom magrittr %>%
-cor_test<-function(x, use.colnames = T){
+cor_test<-function(x, use.colnames = TRUE,self = TRUE){
   if(use.colnames == TRUE){
     comb <- expand.grid(colnames(x),colnames(x))
+    }else{
+    comb <- expand.grid(rownames(x),rownames(x))
+    }
+  if(self == FALSE){
+    comb <- comb[comb$Var1 != comb$Var2, ]
     }
   res <- pmap(comb, ~cor.test(x[[.x]],x[[.y]]))%>%map_df(tidy)%>%cbind(comb,.)
   return(res)
