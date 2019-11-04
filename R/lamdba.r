6d8495819dd41677eff9d2d54b5c0c4873920a02
bioguo <guokai8@gmail.com>
2019-11-04 09:14:06 -0600

    Update lamdba.r

--- R/lamdba.r
+++ R/lamdba.r
@@ -1,1 +1,12 @@
-
+##' @title lambda funtion
+##' @importFrom rlang f_lhs
+##' @importFrom rlang f_env
+##' @importFrom rlang set_names
+lambda <- function(x){
+    arg_names <- all.vars(f_lhs(x))
+    new_function(args = set_names(rep(list(expr()), 
+                                      length(arg_names)), 
+                                  arg_names), 
+                 body = f_rhs(x), 
+                 env = f_env(x))
+}
